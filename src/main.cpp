c7ee94b7359c8d3288a743a402dd9040d67432fb
DevTotoro <totoro.softwaredev@gmail.com>
2024-09-24 15:47:29 +0300

    Add raylib library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <raylib.h>
+
+void UpdateDrawFrame()
+{
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+        UpdateDrawFrame();
+
+    CloseWindow();
+
     return 0;
 }
